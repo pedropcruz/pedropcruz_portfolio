a680b7cb74bf038ee54c772b12c0c9cfd2350eb9
pedropcruz <pmcruz@impresa.pt>
2018-01-15 16:12:58 +0000

    fix margin values on About socialLinks

--- src/containers/About.css.js
+++ src/containers/About.css.js
@@ -1,123 +1,119 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 import variables from "../styles/variables.css";
 
 const AboutCSS = styled.section`
   color: ${variables.colors.primaryColor};
-  h1{
+  h1 {
     margin-top: 6rem;
-    font:900 1.3em/2em ${variables.fonts.Montserrat};
-    @media ${variables.media.sm}{
+    font: 900 1.3em/2em ${variables.fonts.Montserrat};
+    @media ${variables.media.sm} {
       margin-top: 4rem;
-      font:900 3em/1.7em ${variables.fonts.Montserrat};
+      font: 900 3em/1.7em ${variables.fonts.Montserrat};
     }
-    
   }
-  form{
+  form {
     min-height: 385px;
-    h1{
+    h1 {
       font-size: 1.8em;
       letter-spacing: 0;
     }
-    input{
+    input {
       color: ${variables.colors.primaryColor};
       text-align: center;
-      font:400 2em/2.2em ${variables.fonts.Raleway};
+      font: 400 2em/2.2em ${variables.fonts.Raleway};
       background: transparent;
       border: none;
       border-bottom: 4px solid ${variables.colors.primaryColor};
-      &:focus{
+      &:focus {
         outline: 0;
       }
     }
   }
-  article{
-  text-align: center;
-  padding: 4rem 2rem; 
-    @media ${variables.media.sm}{
+  article {
+    text-align: center;
+    padding: 4rem 2rem;
+    @media ${variables.media.sm} {
       text-align: left;
     }
-    h1.name{
+    h1.name {
       font-size: 1.7rem;
       margin: 0;
       letter-spacing: 0;
-      @media ${variables.media.sm}{
+      @media ${variables.media.sm} {
         font-size: 3em;
         margin-top: 2rem;
       }
     }
-    h2.role{
+    h2.role {
       letter-spacing: 0;
       font: 700 1.5em/1.2em ${variables.fonts.MontserratMedium};
     }
-    p.text{
+    p.text {
       font: 400 1.1em/1.2em ${variables.fonts.Raleway};
       text-align: justify;
-      @media ${variables.media.sm}{
-        text-align: inherit;      
+      @media ${variables.media.sm} {
+        text-align: inherit;
       }
     }
   }
-  .socialLinks{
-    span.scroll{
-      left:0;
-      right:0;
-      top:30%;
+  .socialLinks {
+    span.scroll {
+      left: 0;
+      right: 0;
+      top: 30%;
       font: 900 1.2em/1.4em ${variables.fonts.Montserrat};
       text-decoration: none;
       transform: rotate(90deg);
       color: ${variables.colors.primaryColor};
-      z-index:0;
-      opacity: .2;
-      @media ${variables.media.sm}{
-        left:45%;
-        top:initial;
-        right: initial;        
+      z-index: 0;
+      opacity: 0.2;
+      @media ${variables.media.sm} {
+        left: 45%;
+        top: initial;
+        right: initial;
       }
-      &:after{
-        content:"";
+      &:after {
+        content: "";
         position: absolute;
-        top:50%;
-        transform: translate(0,-50%);
+        top: 50%;
+        transform: translate(0, -50%);
         display: inline-block;
         height: ${variables.value.bv3};
-        width:11em;
-        margin-left: ${variables.value.bv3} ;
+        width: 11em;
+        margin-left: ${variables.value.bv3};
         background: ${variables.colors.primaryColor};
       }
     }
-    ul{
+    ul {
       text-align: center;
-      @media ${variables.media.md}{
+      @media ${variables.media.md} {
         text-align: right;
       }
-      li{
+      li {
         display: block;
-        margin:1.5rem 0;
-        @media ${variables.media.sm}{
+        margin: 1.5rem 0;
+        @media ${variables.media.sm} {
           display: inline-block;
           margin: 0;
         }
-        a{
+        a {
           color: ${variables.colors.primaryColor};
           text-decoration: none;
-          @media ${variables.media.sm}{
-            margin:0 1rem;
+          @media ${variables.media.sm} {
+            margin: 0 2vw;
           }
-          @media ${variables.media.md}{
-            margin:0 1.5rem;
+          @media ${variables.media.lg} {
+            margin: 0 3.5vw;
           }
-          @media ${variables.media.lg}{
-            margin:0 3.5rem;
-          }
-          &:focus{
+          &:focus {
             outline: 0;
           }
-          &:visited{
-            color: ${variables.colors.primaryColor}; 
+          &:visited {
+            color: ${variables.colors.primaryColor};
           }
-        }  
+        }
       }
-    } 
+    }
   }
 `;
 
