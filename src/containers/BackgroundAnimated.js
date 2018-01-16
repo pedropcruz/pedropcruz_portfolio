e71e03dbb56e16432cf18f5820e83be68d185bf0
pedropcruz <pmcruz@impresa.pt>
2018-01-16 04:09:47 +0000

    refactor setTimeOut and setInterval

--- src/containers/BackgroundAnimated.js
+++ src/containers/BackgroundAnimated.js
@@ -13,47 +13,56 @@ export default class BackgroundAnimated extends Component {
     this.LoadSolidTypes = this.LoadSolidTypes.bind(this);
   }
 
-  componentDidMount(){
-      this.LoadSolidTypes();
-  }
+  componentDidMount() {
+    this.LoadSolidTypes();
 
-  LoadSolidTypes() {
-    for(let i = 0; i < num; i++){
-        let type = solidType[this.RandomNumber(0, solidType.length-1)];
-
-        let style = {
-            top: this.RandomNumber(100, (window.innerHeight - 100)) + 'px',
-            left: this.RandomNumber(100, (window.innerWidth - 100)) + 'px',
-            transform: 'rotate(' + this.RandomNumber(0, 360) + 'deg',
-        };
-        
-        let div = document.createElement('div');
-        div.className += type + ' outer';
-        div.setAttribute('data-idx', i);
-        document.querySelector('.backgroundAnimated').append(div);
-        
-        let $el = document.querySelector('div.outer[data-idx="' + i + '"]');
-        Object.assign($el.style, style);
-    }
-    setTimeout(()=>{this.LetSolidDance();}, 1);
-    setInterval(()=>{
-        this.LetSolidDance();
+    this.timer = setTimeout(() => {
+      this.LetSolidDance();
+    }, 1);
+    this.timerInterval = setInterval(() => {
+      this.LetSolidDance();
     }, 5000);
   }
 
-  LetSolidDance(){
+  componentWillUnmount(){
+      clearTimeout(this.timer);
+      clearTimeout(this.timerInterval);
+  }
+
+  LoadSolidTypes() {
     for (let i = 0; i < num; i++) {
-        let div = document.querySelector('div.outer[data-idx="' + i + '"]');
-        Object.assign(div.style, {
-          transform:
-            "translate(" +
-            this.RandomNumber(-1000, 3000) +
-            "px, " +
-            this.RandomNumber(-1000, 3000) +
-            "px) rotate(" +
-            this.RandomNumber(0, 3000) + 'deg)'
-        });
-      }
+      let type = solidType[this.RandomNumber(0, solidType.length - 1)];
+
+      let style = {
+        top: this.RandomNumber(100, window.innerHeight - 100) + "px",
+        left: this.RandomNumber(100, window.innerWidth - 100) + "px",
+        transform: "rotate(" + this.RandomNumber(0, 360) + "deg"
+      };
+
+      let div = document.createElement("div");
+      div.className += type + " outer";
+      div.setAttribute("data-idx", i);
+      document.querySelector(".backgroundAnimated").append(div);
+
+      let $el = document.querySelector('div.outer[data-idx="' + i + '"]');
+      Object.assign($el.style, style);
+    }
+  }
+
+  LetSolidDance() {
+    for (let i = 0; i < num; i++) {
+      let div = document.querySelector('div.outer[data-idx="' + i + '"]');
+      Object.assign(div.style, {
+        transform:
+          "translate(" +
+          this.RandomNumber(-1000, 3000) +
+          "px, " +
+          this.RandomNumber(-1000, 3000) +
+          "px) rotate(" +
+          this.RandomNumber(0, 3000) +
+          "deg)"
+      });
+    }
   }
 
   RandomNumber(from, to) {
@@ -62,9 +71,7 @@ export default class BackgroundAnimated extends Component {
 
   render() {
     return (
-      <BackgroundAnimatedCSS className="absolute top-0 bottom-0 right-0 left-0 backgroundAnimated">
-
-      </BackgroundAnimatedCSS>
+      <BackgroundAnimatedCSS className="absolute top-0 bottom-0 right-0 left-0 backgroundAnimated" />
     );
   }
 }
