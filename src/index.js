090e58f7d1eb404b3f6191abf87aeb1f47971475
pedropcruz <pmcruz@impresa.pt>
2018-03-24 00:45:16 +0000

    remove react document meta from project && insert meta on html

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import "./styles/global.css";
 import App from "./containers/App";
 import Resume from "./containers/Resume/Resume";
 import {BrowserRouter as Router, Route} from "react-router-dom";
-import DocumentMeta from 'react-document-meta';
-import imageForFacebookMeta from './resources/facebook_share_image_dialog.jpg';
 import registerServiceWorker from "./registerServiceWorker";
 
 class Root extends Component {
@@ -20,34 +18,13 @@ class Root extends Component {
     }
 
     render() {
-
-        const meta = {
-            description: 'My Own Portfolio, check it out! Click Here dude! Feedback are welcome!',
-            canonical: 'pedropcruz.pt',
-            meta: {
-                charset: 'utf-8',
-                name: {
-                    keywords: 'front-end, frontend, designer, front-end developer, developer, coder, coding, programming,'
-                },
-                property: {
-                    'og:image': imageForFacebookMeta,
-                    'og:title': 'pedropcruz personal website',
-                    'og:url': 'http://pedropcruz.pt/resume',
-                    'og:site_name': 'Pedropcruz Website',
-                    'og:type': 'portfolio',
-                }
-            }
-        };
-
         return (
-            <DocumentMeta {...meta} extend>
-                <Router>
-                    <div>
-                        <Route exact path="/" component={App}/>
-                        <Route path="/resume" component={Resume}/>
-                    </div>
-                </Router>
-            </DocumentMeta>
+            <Router>
+                <div>
+                    <Route exact path="/" component={App}/>
+                    <Route path="/resume" component={Resume}/>
+                </div>
+            </Router>
         );
     }
 }
