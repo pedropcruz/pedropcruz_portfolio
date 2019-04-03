d726f781c31497fbc747db3b92a21a4e8ab13ee8
pedropcruz <pedropcruz@outlook.pt>
2019-04-03 17:12:47 +0100

    updated

--- src/index.js
+++ src/index.js
@@ -1,33 +1,33 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import ReactGA from "react-ga";
 import "./styles/global.css";
 import App from "./containers/App";
 import Resume from "./containers/Resume/Resume";
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import registerServiceWorker from "./registerServiceWorker";
 
 class Root extends Component {
 
-    constructor() {
-        super();
+	constructor() {
+		super();
 
-        ReactGA.initialize('UA-116283572-1');
+		ReactGA.initialize('UA-116283572-1');
 
-        ReactGA.pageview(window.location.pathname + window.location.search)
-    }
+		ReactGA.pageview(window.location.pathname + window.location.search)
+	}
 
-    render() {
-        return (
-            <Router>
-                <div>
-                    <Route exact path="/" component={App}/>
-                    <Route path="/resume" component={Resume}/>
-                </div>
-            </Router>
-        );
-    }
+	render() {
+		return (
+			<Router>
+				<div>
+					<Route exact path="/" component={App} />
+					<Route path="/resume" component={Resume} />
+				</div>
+			</Router>
+		);
+	}
 }
 
-ReactDOM.render(<Root/>, document.getElementById("root"));
+ReactDOM.render(<Root />, document.getElementById("root"));
 registerServiceWorker();
